fa473b8b92701a678845ad6e31bd09f10c24456b
Donald Stolz <dstolz@e1z2r3p12.42.us.org>
2018-10-05 22:22:50 -0700

    Finishing section 5

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -49,6 +49,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
     });
 });
 
