0e9387c7f09b8314e7c4e2163f812f44a5e94cfa
Donald Stolz <dstolz@e1z3r2p17.42.us.org>
2018-10-06 10:37:11 -0700

    Finished lecture 55

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,3 +1,19 @@
 module.exports.add = (a, b) => a + b;
 
+module.exports.asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+};
+
 module.exports.square = (a) => a * a;
+
+module.exports.asyncSquare = (a, callback) => {
+    setTimeout(() => {
+        callback(a * a);
+    }, 1000);
+};
+
+module.exports.createUser = (name, age, location) => {
+    return {name, age, location}
+};
