8cfa932f29430a1df4f27c14bcd8458651a89cc0
Donald Stolz <donaldtstolz@gmail.com>
2018-10-06 22:11:32 -0700

    Finished server testing with jest

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -1,9 +1,22 @@
 const express	= require('express');
+const utils = require('../utils/utils');
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello world!\n');	
+	res.status(404).send({
+        error: 'Page not found',
+        name: 'Todo App v1.0'
+    });
 })
 
+app.get('/users', (req, res) => {
+    let users = [];
+    for (var i = 0; i < 5; i++) {
+        users[i] = utils.createUser('User '+i, ((35 * i + 3) % 30), 'Chicago');
+    };
+    res.send(users);
+});
+
 app.listen(3000);
+module.exports.app = app;
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,4 +1,3 @@
-const expect = require('expect');
 const utils = require('./utils');
 
 test('Should add 33 to 11', () => {
@@ -13,18 +12,18 @@ test('Should async add 42 to 21', (done) => {
     utils.asyncAdd(42, 21, (sum) => {
         expect(sum).toBe(63);
         done();
-    })    
+    })
 })
 
 test('Should square 42', (done) => {
     utils.asyncSquare(42, (square) => {
         expect(square).toBe(1764);
         done();
-    })    
+    })
 })
 
 test('Should create a new user', () => {
-    // expect({name:"Andrew"}).toEqual({name:"Andrew"});   
+    // expect({name:"Andrew"}).toEqual({name:"Andrew"});
     // expect([2,3,4]).toContain(3);
     expect(utils.createUser('Andrew', 25, 'Chicago'))
     .toMatchObject({
